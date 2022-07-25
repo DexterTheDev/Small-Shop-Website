c49bc5b717ea5227dcd4deae0a163f284bd57784
Dexter <63425800+DexterTheDev@users.noreply.github.com>
2022-07-25 15:00:42 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                purple: {
+                    100: "#292751",
+                    200: "#3832ad",
+                    300: "#2d3d5d",
+                    400: "#353364"
+                },
+                gold: {
+                    100: "#FCB900",
+                    200: "#BBA60A",
+                    300: "#A69729",
+                    400: "#C79818",
+                    500: "#eab112"
+                }
+            },
+            borderStyle: ['hover']
+        },
+        fontFamily: {
+            body: ["Nunito"]
+        }
+    }
+}
